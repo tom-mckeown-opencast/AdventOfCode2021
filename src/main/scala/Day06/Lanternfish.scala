4a07edd627272424dedd33f22600ff02b619b520
Thomas McKeown <tom.mckeown@opencastsoftware.com>
2021-12-08 15:07:05 +0000

    Day06 solution

---
+++ src/main/scala/Day06/Lanternfish.scala
@@ -0,0 +1,83 @@
+package Day06
+
+import scala.collection.mutable.ArrayBuffer
+
+object Lanternfish {
+  // This function will get the sequence of fish after a number of days
+  // WARNING: This function will run out of memory after a certain point
+  def getFishSequence(data: String, days: Int): Array[Int] = {
+    def runDay(fish: ArrayBuffer[Int], stopIndex: Int, index: Int = 0): ArrayBuffer[Int] = {
+      if (index >= stopIndex) {
+        return fish
+      }
+
+      fish(index) -= 1
+      if (fish(index) < 0) {
+        fish(index) = 6
+        fish += 8
+      }
+
+      runDay(fish, stopIndex, index + 1)
+    }
+    def simulateRecursive(fish: ArrayBuffer[Int], days: Int, index: Int = 0): Array[Int] = {
+      if (index % 7 == 0) println()
+      if (index >= days) {
+        return fish.toArray
+      }
+      simulateRecursive(runDay(fish, fish.length), days, index + 1)
+    }
+
+    simulateRecursive(data.split(',').map(_.toInt).to(ArrayBuffer), days)
+  }
+
+  // This function will calculate only the number of fish after a number of days
+  def getFishCount(data: String, days: Int): Long = {
+    def getWeekCount(fish: String, week: Int, index: Int = 0, acc: Array[Long] = Array.ofDim[Long](9)): Array[Long] = {
+      if (index > week) {
+        return acc
+      }
+
+      val newAcc = Array.ofDim[Long](9)
+      if (index == 0) {
+        newAcc(0) = fish.count(c => c == '0')
+        newAcc(1) = fish.count(c => c == '1')
+        newAcc(2) = fish.count(c => c == '2')
+        newAcc(3) = fish.count(c => c == '3')
+        newAcc(4) = fish.count(c => c == '4')
+        newAcc(5) = fish.count(c => c == '5')
+        newAcc(6) = fish.count(c => c == '6')
+        newAcc(7) = fish.count(c => c == '7')
+        newAcc(8) = fish.count(c => c == '8')
+      } else {
+        newAcc(0) = acc(0) + acc(7)
+        newAcc(1) = acc(1) + acc(8)
+        newAcc(2) = acc(2) + acc(0)
+        newAcc(3) = acc(3) + acc(1)
+        newAcc(4) = acc(4) + acc(2)
+        newAcc(5) = acc(5) + acc(3)
+        newAcc(6) = acc(6) + acc(4)
+        newAcc(7) = acc(5)
+        newAcc(8) = acc(6)
+      }
+
+      getWeekCount(fish, week, index + 1, newAcc)
+    }
+    def getDayCount(weekCount: Array[Long], day: Int, index: Int = 0, acc: Long): Long = {
+      if (index > day) {
+        return acc
+      }
+      getDayCount(weekCount, day, index + 1, acc + weekCount(index))
+    }
+
+    val week: Int = (days - 1) / 7
+    val day: Int = (days - 1) % 7
+
+    val weekCount = getWeekCount(data, week)
+    getDayCount(weekCount, day, acc = weekCount.sum)
+  }
+
+  // Get the week number (days / 7)
+  // Get the day number (days % 7)
+  // Calc 0-8s for that week
+  // => recursively go through weeks
+}
---
+++ src/test/scala/Day06/LanternfishTests.scala
@@ -0,0 +1,69 @@
+package Day06
+
+import AoC.Utils
+import org.scalatest.funspec.AnyFunSpec
+
+class LanternfishTests extends AnyFunSpec {
+  val exampleData = "3,4,3,1,2"
+  val taskData = Utils.readFile("src/test/scala/Day06/input.txt")(0)
+  // Lanternfish represented by a number
+  // => That number = number of days until it reproduces
+  // => 6 is the start of the cycle
+  // => 0 is the end of the cycle
+  // => 8 is the start of the FIRST cycle
+  // Lanternfish reproduce at a set rate
+  // => Each fish has its own countdown
+  // => Standard rate = 1 / 7days
+  // => First cycle = 1 / 9days
+  //    => New Lanternfish have to wait an extra 2 days for their first cycle (1 / 9days ==> 1 / 7days)
+
+  describe("Simulation tests") {
+    describe("After 1 day of simulating the example data") {
+      it("Should return an array (2,3,2,0,1)") {
+        val expected = Array(2,3,2,0,1)
+
+        assert(Lanternfish.getFishSequence(exampleData, 1) sameElements expected)
+      }
+    }
+    describe("After 2 days of simulating the example data") {
+      it("Should return an array (1,2,1,6,0,8)") {
+        val expected = Array(1,2,1,6,0,8)
+
+        assert(Lanternfish.getFishSequence(exampleData, 2) sameElements expected)
+      }
+    }
+    describe("After 3 days of simulating the example data") {
+      it("Should return an array (0,1,0,5,6,7,8)") {
+        val expected = Array(0,1,0,5,6,7,8)
+
+        assert(Lanternfish.getFishSequence(exampleData, 3) sameElements expected)
+      }
+    }
+    describe("After 18 days of simulating the example data") {
+      it("Should return 26") {
+        assert(Lanternfish.getFishCount(exampleData, 18) == 26)
+      }
+    }
+    describe("After 21 days of simulating the example data") {
+      it("Should return 37") {
+        assert(Lanternfish.getFishCount(exampleData, 21) == 37)
+      }
+    }
+    describe("After 80 days of simulating the example data") {
+      it("Should return 5934") {
+        assert(Lanternfish.getFishCount(exampleData, 80) == 5934)
+      }
+    }
+
+    describe("After 80 days of simulating the task data") {
+      it("Should return 363101") {
+        assert(Lanternfish.getFishCount(taskData, 80) == 363101)
+      }
+    }
+    describe("After 256 days of simulating the task data") {
+      it("Should return 1644286074024") {
+        assert(Lanternfish.getFishCount(taskData, 256) == 1644286074024L)
+      }
+    }
+  }
+}
